869dcd57019951ba5fb53448ffcca1466919a7c4
Amadé Hegedüs <amade.hegedus@gmail.com>
2022-04-10 21:37:12 +0200

    ViewModel functions

--- app/src/main/java/hu/bme/aut/moblab/birdrepellent/ui/main/MainViewModel.kt
+++ app/src/main/java/hu/bme/aut/moblab/birdrepellent/ui/main/MainViewModel.kt
@@ -1,4 +1,28 @@
 package hu.bme.aut.moblab.birdrepellent.ui.main
 
-class MainViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import hu.bme.aut.moblab.birdrepellent.model.HarmfulBird
+import hu.bme.aut.moblab.birdrepellent.persistence.AppDatabase
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val repository: MainRepository
+) : ViewModel() {
+
+    fun getAllHarmfulBirds(): LiveData<List<HarmfulBird>> {
+        return repository.getAllHarmfulBirds();
+    }
+
+    fun addHarmfulBird(harmfulBird: HarmfulBird) {
+        repository.addHarmfulBird(harmfulBird);
+    }
+
+    fun deleteHarmfulBird(harmfulBird: HarmfulBird) {
+        repository.deleteHarmfulBird(harmfulBird);
+    }
 }
