4e48c8291957f114b2737cd9776243e70603d782
Amadé Hegedüs <amade.hegedus@gmail.com>
2022-04-10 21:37:48 +0200

    Service functions

--- app/src/main/java/hu/bme/aut/moblab/birdrepellent/network/SyncService.kt
+++ app/src/main/java/hu/bme/aut/moblab/birdrepellent/network/SyncService.kt
@@ -1,4 +1,22 @@
 package hu.bme.aut.moblab.birdrepellent.network
 
-class SyncService {
+import hu.bme.aut.moblab.birdrepellent.dto.HarmfulBirdDto
+import retrofit2.Call
+import retrofit2.http.*
+
+interface SyncService {
+    @PUT
+    fun putHarmfulBirds(@Query("deviceId") deviceId: Long, @Body harmfulBirds: List<HarmfulBirdDto>): Call<List<HarmfulBirdDto>>
+
+    @PUT
+    fun putHarmfulBird(@Query("deviceId") deviceId: Long, @Body harmfulBird: HarmfulBirdDto): Call<HarmfulBirdDto>
+
+    @GET
+    fun getAllHarmfulBirds(@Query("deviceId") deviceId: Long);
+
+    @DELETE
+    fun deleteHarmfulBird(@Query("deviceId") deviceId: Long, @Body harmfulBird: HarmfulBirdDto);
+
+    @DELETE
+    fun deleteAllHarmfulBirds(@Query("deviceId") deviceId: Long);
 }
