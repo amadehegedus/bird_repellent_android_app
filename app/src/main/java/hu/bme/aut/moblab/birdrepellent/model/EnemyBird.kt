df23a344c927298ac0bb3f4f197bf516ed722588
Amadé Hegedüs <amade.hegedus@gmail.com>
2022-04-10 19:06:11 +0200

    Extend db models

--- app/src/main/java/hu/bme/aut/moblab/birdrepellent/model/EnemyBird.kt
+++ app/src/main/java/hu/bme/aut/moblab/birdrepellent/model/EnemyBird.kt
@@ -1,5 +1,13 @@
 package hu.bme.aut.moblab.birdrepellent.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class EnemyBird(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name="id", index = true)
+    val id: Long,
     val name: String
 )
--- app/src/main/java/hu/bme/aut/moblab/birdrepellent/model/HarmfulBird.kt
+++ app/src/main/java/hu/bme/aut/moblab/birdrepellent/model/HarmfulBird.kt
@@ -1,6 +1,15 @@
 package hu.bme.aut.moblab.birdrepellent.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
 data class HarmfulBird(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name="id", index = true)
+    val id: Long,
     val name: String,
-    val enemies: List<EnemyBird>
+    val enemies: List<EnemyBird>,
+    val active: Boolean
 )
