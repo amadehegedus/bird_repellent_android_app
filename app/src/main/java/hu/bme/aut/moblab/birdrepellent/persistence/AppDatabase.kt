58582bba2ef08659f4ba48897b6428ced2e6b499
Amadé Hegedüs <amade.hegedus@gmail.com>
2022-04-24 14:23:52 +0200

    Implement db

--- app/src/main/java/hu/bme/aut/moblab/birdrepellent/persistence/AppDatabase.kt
+++ app/src/main/java/hu/bme/aut/moblab/birdrepellent/persistence/AppDatabase.kt
@@ -1,4 +1,21 @@
 package hu.bme.aut.moblab.birdrepellent.persistence
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import hu.bme.aut.moblab.birdrepellent.di.ApplicationScope
+import hu.bme.aut.moblab.birdrepellent.persistence.model.HarmfulBird
+import kotlinx.coroutines.CoroutineScope
+import javax.inject.Inject
+import javax.inject.Provider
+
+@Database(entities = [HarmfulBird::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun getHarmfulBirdDao(): HarmfulBirdDao
+
+    class Callback @Inject constructor(
+        private val database: Provider<AppDatabase>,
+        @ApplicationScope private val applicationScope: CoroutineScope
+    ): RoomDatabase.Callback()
 }
