7b125dabc4c857507030ba0fc243d41f0e484681
Amadé Hegedüs <amade.hegedus@gmail.com>
2022-04-24 14:24:39 +0200

    Implement persistence module

--- app/src/main/java/hu/bme/aut/moblab/birdrepellent/di/PersistenceModule.kt
+++ app/src/main/java/hu/bme/aut/moblab/birdrepellent/di/PersistenceModule.kt
@@ -1,10 +1,26 @@
 package hu.bme.aut.moblab.birdrepellent.di
 
+import android.app.Application
+import android.app.SharedElementCallback
+import androidx.room.Room
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import hu.bme.aut.moblab.birdrepellent.persistence.AppDatabase
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object PersistenceModule {
+    @Provides
+    @Singleton
+    fun provideAppDatabase(application: Application, callback: AppDatabase.Callback) = Room
+        .databaseBuilder(application, AppDatabase::class.java, "app_database")
+        .fallbackToDestructiveMigration()
+        .addCallback(callback)
+        .build()
+
+    @Provides
+    fun provideHarmfulBirdDao(database: AppDatabase) = database.getHarmfulBirdDao()
 }
