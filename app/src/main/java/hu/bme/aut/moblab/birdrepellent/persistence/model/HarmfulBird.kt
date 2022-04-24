6f5c121412f6d0c54eae07272309e6e93b9ce643
Amadé Hegedüs <amade.hegedus@gmail.com>
2022-04-24 02:52:33 +0200

    Models created

--- app/src/main/java/hu/bme/aut/moblab/birdrepellent/persistence/model/HarmfulBird.kt
+++ app/src/main/java/hu/bme/aut/moblab/birdrepellent/persistence/model/HarmfulBird.kt
@@ -3,11 +3,11 @@ package hu.bme.aut.moblab.birdrepellent.persistence.model
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity
+@Entity(tableName = "harmful_bird")
 data class HarmfulBird(
     @PrimaryKey(autoGenerate = true)
-    var id: Long,
-    var name: String,
-    var active: Boolean,
-    var modified: Long
+    var id : Int,
+    var name: String = "",
+    var enemies: List<String> = listOf(),
+    var active: Boolean = true
 )
