16e9011685f8e36d5b230f7d63a2db1d7050c179
Amadé Hegedüs <amade.hegedus@gmail.com>
2022-04-24 14:22:55 +0200

    Implement HarmfulBirdDao

--- app/src/main/java/hu/bme/aut/moblab/birdrepellent/persistence/HarmfulBirdDao.kt
+++ app/src/main/java/hu/bme/aut/moblab/birdrepellent/persistence/HarmfulBirdDao.kt
@@ -1,5 +1,23 @@
 package hu.bme.aut.moblab.birdrepellent.persistence
 
-interface HarmfulBirdDao {
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import hu.bme.aut.moblab.birdrepellent.persistence.model.HarmfulBird
 
+@Dao
+interface HarmfulBirdDao {
+    @Query("SELECT * FROM harmful_bird")
+    fun getHarmfulBirds(): LiveData<List<HarmfulBird>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertHarmfulBird(harmfulBird: HarmfulBird): Long
+
+    @Update
+    fun updateHarmfulBird(harmfulBird: HarmfulBird)
+
+    @Delete
+    fun deleteHarmfulBird(harmfulBird: HarmfulBird)
+
+    @Query("DELETE FROM harmful_bird")
+    fun deleteAllHarmfulBirds()
 }
