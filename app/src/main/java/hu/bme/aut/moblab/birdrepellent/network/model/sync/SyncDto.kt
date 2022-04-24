8eb9130e495edb56ae2f1a7b4f82f65a82e645d5
Amadé Hegedüs <amade.hegedus@gmail.com>
2022-04-24 14:22:32 +0200

    Change type of model ids to Long

--- app/src/main/java/hu/bme/aut/moblab/birdrepellent/network/model/sync/SyncDto.kt
+++ app/src/main/java/hu/bme/aut/moblab/birdrepellent/network/model/sync/SyncDto.kt
@@ -1,7 +1,7 @@
 package hu.bme.aut.moblab.birdrepellent.network.model.sync
 
 data class HarmfulBirdDto (
-    var id : Int,
+    var id : Long,
     var name: String = "",
     var enemies: List<String> = listOf(),
     var active: Boolean = true
