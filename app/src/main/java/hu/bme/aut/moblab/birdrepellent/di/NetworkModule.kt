e7bbfbe0314eb1a20b1e1a1a63961451d0331182
Amadé Hegedüs <amade.hegedus@gmail.com>
2022-04-24 02:53:15 +0200

    Create network module

--- app/src/main/java/hu/bme/aut/moblab/birdrepellent/di/NetworkModule.kt
+++ app/src/main/java/hu/bme/aut/moblab/birdrepellent/di/NetworkModule.kt
@@ -1,10 +1,39 @@
 package hu.bme.aut.moblab.birdrepellent.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import hu.bme.aut.moblab.birdrepellent.network.XenoCantoApi
+import hu.bme.aut.moblab.birdrepellent.util.XENO_CANTO_BASE_URL
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideLoggingInterceptor() = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor) = OkHttpClient.Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient) = Retrofit.Builder()
+        .baseUrl(XENO_CANTO_BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(okHttpClient)
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideXenoCantoApi(retrofit: Retrofit) = retrofit.create(XenoCantoApi::class.java)
 }
