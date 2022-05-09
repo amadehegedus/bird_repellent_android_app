48b5a78a110c0caa1180f4c1883afdd4f0b0071f
Amadé Hegedüs <amade.hegedus@gmail.com>
2022-05-09 06:45:15 +0200

    :angry:

--- app/src/androidTest/java/hu/bme/aut/moblab/birdrepellent/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/hu/bme/aut/moblab/birdrepellent/ExampleInstrumentedTest.kt
@@ -1,24 +1,56 @@
 package hu.bme.aut.moblab.birdrepellent
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import hu.bme.aut.moblab.birdrepellent.persistence.AppDatabase
+import hu.bme.aut.moblab.birdrepellent.persistence.HarmfulBirdDao
+import hu.bme.aut.moblab.birdrepellent.persistence.model.HarmfulBird
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.runTest
+import org.junit.After
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import javax.xml.transform.Source
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
+
+@ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
+@SmallTest
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("hu.bme.aut.moblab.birdrepellent", appContext.packageName)
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var db: AppDatabase
+    private lateinit var harmfulBirdDao: HarmfulBirdDao
+
+    @Before
+    fun init() {
+        db = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            AppDatabase::class.java
+        ).allowMainThreadQueries().build()
+
+        harmfulBirdDao = db.getHarmfulBirdDao()
+
+        val harmfulBird = HarmfulBird(1, "1", mutableListOf("1"), true)
+        runTest {
+            harmfulBirdDao.insertHarmfulBird(harmfulBird)
+        }
+    }
+
+    @After
+    fun closeDb(){
+        db.close()
     }
 }
